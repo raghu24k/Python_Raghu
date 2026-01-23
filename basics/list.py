544071446ea1a7758b95de4357f57f02fedcc7f3
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-23 23:40:59 +0530

    learning list

--- basics/list.py
+++ basics/list.py
@@ -1,8 +1,10 @@
 a = [1,2,3,4,'hi','raghu']
-print('the length of list a is:', len(a),'and list is: ',a)
+# print('the length of list a is:', len(a),'and list is: ',a)
 
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 # making list using "list()" 
-b = list(('raghu','bhavy',1,2))
-print(type(b),b)
+# b = list(('raghu','bhavy',1,2))
+# print(type(b),b)
+
+print(a[1],a[-1],a[2:],a[-4:-1])
