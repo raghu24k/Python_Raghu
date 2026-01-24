887a84d286e64f7391b769b2471c47f56c59d9e7
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-25 00:26:13 +0530

    sort

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path , HTTPException
+from fastapi import FastAPI, Path , HTTPException, Query
 import json
 
 patient = FastAPI()
@@ -23,3 +23,18 @@ def ViewPatient(patient_id: str = Path(...,description="id of the patient in the
         if p.get("patient_id") == patient_id:
             return p
     raise HTTPException(status_code = 404, detail="patient not found")
+
+
+@patient.get('/sort')
+def sort_patient(sort_by:str=Query(...,description="sort on the basis of age, height"), order:str = Query('asc',description="sort in asc or desc order")):
+    vaild_field = ['age','height']
+    if sort_by not in vaild_field:
+        raise HTTPException(Status_code=404,detail="Invaild field select from {Vaild_fields}")
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail='Invaild order select between asc and desc')
+
+    data = LoadData()
+    sort_order = True if order =='desc' else False
+    sorted_data = sorted(data.values(),key=lambda x: x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
