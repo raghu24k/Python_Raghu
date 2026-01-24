8a8373a505c9e93565736f7c15e33cf7a445ece9
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-24 22:31:58 +0530

    still learning

--- fastapi/main.py
+++ fastapi/main.py
@@ -12,3 +12,14 @@ def LoadData():
 def view():
     data = LoadData()
     return data
+
+@patient.get('/patient/{patient_id}')
+def ViewPatient(patient_id: str):
+    # load all the patients
+    data = LoadData()
+    # Access the list of patients
+    patients = data.get("patients", [])
+    for p in patients:
+        if p.get("patient_id") == patient_id:
+            return p
+    return {"message": "the patient is not found"}
