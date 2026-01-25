5dc1cb2042a61f4656b6420142eba93c20275cef
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-25 19:43:46 +0530

    field validatior

--- pydantic_learning/field_validator.py
+++ pydantic_learning/field_validator.py
@@ -1,35 +1,42 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional, Annotated
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from typing import List, Dict, Optional
 class patient(BaseModel):
-    name: Annotated[str, Field(max_length=50, title='Name of the patient',
-    description='give the name of the patient in less then 50 charc',
-    example= ['Nitesh','Amit'])]
+    name: str
     email: EmailStr
-    url: AnyUrl
-    age: int = Field(gt=0,lt=100)
-    weight: float = Field(gt = 0)
-    married: Annotated[bool, Field(defaul=None, description='Is the patient married' \
-    'or not')]
-    allergies:Optional[list[str]] = None
+    # url: AnyUrl
+    age: int 
+    weight: float 
+    married: bool
+    allergies:list[str]
     contact_details: dict[str, str]
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        valid_domains = ['hdfc.com','icici.com']
+        domain_name = value.split('@')[-1]
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid domain')
+        return value
 
 def insert_patient(patient: patient):
     print(patient.name)
     print(patient.age)
     print(patient.email)
-    print(patient.url)
+    # print(patient.url)
     print('inserted')
 
 def update_patient(patient: patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
     print('updated')
     # print(patient.allergies)
 
 patient_info = {
     'name': 'raghuwinder',
-    'email': 'abc@gmail.com',
-    'url': 'https://www.google.com',
+    'email': 'abc@icici.com',
+    # 'url': 'https://www.google.com',
     'age': '32',
     'weight': '77',
     'married': True,
