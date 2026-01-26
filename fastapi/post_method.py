417ad7f4dde52454d26f90a553117a88c286c627
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-26 22:19:28 +0530

    making post method using pydantic

--- fastapi/post_method.py
+++ fastapi/post_method.py
@@ -2,17 +2,46 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-@app.post('/create/name')
-def create_user():
-    return{'message':'You have successfully created user'}
+# @app.post('/create/name')
+# def create_user():
+#     return{'message':'You have successfully created user'}
 
+
+users = []
 class user(BaseModel):
     name: str
     age: int
 @app.post('/user')
 def create_user(user: user):
+    users.append(user)
     return{
-        'name': user.name,
-        'age': user.age,
-        'message': 'User Created '
+        # 'name': user.name,
+        # 'age': user.age,
+        'message': 'User Created ',
+        'total user': len(users)
     }
+
+@app.get('/user')
+def get_users():
+    return users
+
+
+
+studentlist = []   # 👈 storage (RAM)
+
+class AddStudent(BaseModel):   # 👈 Model
+    student_name: str
+    student_age: int
+    student_class: int
+
+@app.post("/addstudent")
+def add_student(student: AddStudent):
+    studentlist.append(student)   
+    return {
+        "student": student,
+        "status": "student added",
+        "total_students": len(studentlist)
+    }
+@app.get('/student')
+def getstudents():
+    return studentlist
